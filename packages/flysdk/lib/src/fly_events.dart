eaccb5be780e67e43b3eaac4f97bedf8f7a3597e
Hariram <hariram.c@contus.in>
2022-11-23 23:08:30 +0530

    Created package for flutter files

---
+++ packages/flysdk/lib/src/fly_events.dart
@@ -0,0 +1,85 @@
+import 'dart:convert';
+
+import 'package:flysdk/src/fly_chat.dart';
+
+
+
+abstract class FlyEvents {
+
+  initListeners(){
+    FlyChat.onMessageReceived.listen(onMessageReceived);
+    FlyChat.onMessageStatusUpdated.listen(onMessageStatusUpdated);
+    FlyChat.onMediaStatusUpdated.listen(onMediaStatusUpdated);
+
+    FlyChat.onGroupProfileFetched.listen(onGroupProfileFetched);
+    FlyChat.onNewGroupCreated.listen(onNewGroupCreated);
+    FlyChat.onGroupProfileUpdated.listen(onGroupProfileUpdated);
+    FlyChat.onNewMemberAddedToGroup.listen(onNewMemberAddedToGroup);
+    FlyChat.onMemberRemovedFromGroup.listen(onMemberRemovedFromGroup);
+    FlyChat.onFetchingGroupMembersCompleted.listen(onFetchingGroupMembersCompleted);
+    FlyChat.onDeleteGroup.listen(onDeleteGroup);
+    FlyChat.onFetchingGroupListCompleted.listen(onFetchingGroupListCompleted);
+    FlyChat.onMemberMadeAsAdmin.listen(onMemberMadeAsAdmin);
+    FlyChat.onMemberRemovedAsAdmin.listen(onMemberRemovedAsAdmin);
+    FlyChat.onLeftFromGroup.listen((event){
+      if(event!=null) {
+        var data = json.decode(event.toString());
+        var groupJid = data["groupJid"] ?? "";
+        var leftUserJid = data["leftUserJid"] ?? "";
+        onLeftFromGroup(groupJid: groupJid,userJid: leftUserJid);
+      }
+    });
+    FlyChat.onGroupNotificationMessage.listen(onGroupNotificationMessage);
+    FlyChat.onGroupDeletedLocally.listen(onGroupDeletedLocally);
+  }
+
+  void onMessageReceived(chatMessage){
+    //Log("flutter onMessageReceived", ChatMessage.toString());
+  }
+  void onMessageStatusUpdated(event){
+    //Log("flutter onMessageStatusUpdated", event.toString());
+  }
+  void onMediaStatusUpdated(event){
+
+  }
+  void onGroupProfileFetched(groupJid){
+
+  }
+  void onNewGroupCreated(groupJid){
+
+  }
+  void onGroupProfileUpdated(groupJid){
+  }
+  void onNewMemberAddedToGroup(event){
+
+  }
+  void onMemberRemovedFromGroup(event){
+
+  }
+  void onFetchingGroupMembersCompleted(groupJid){
+
+  }
+  void onDeleteGroup(groupJid){
+
+  }
+  void onFetchingGroupListCompleted(noOfGroups){
+
+  }
+  void onMemberMadeAsAdmin(event){
+
+  }
+  void onMemberRemovedAsAdmin(event){
+
+  }
+  void onLeftFromGroup({required String groupJid,required String userJid}){
+
+  }
+  void onGroupNotificationMessage(event){
+
+  }
+  void onGroupDeletedLocally(groupJid){
+
+  }
+}
+
+
