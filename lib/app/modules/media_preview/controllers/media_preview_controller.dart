2d8e55ae4f1fc9514cb0d51689bad7a63445727d
SaravanakumarB_Contus <saravanakumar.b@contus.in>
2023-02-11 12:57:17 +0530

    keyboard showing after app foreground in media preview screen

--- lib/app/modules/media_preview/controllers/media_preview_controller.dart
+++ lib/app/modules/media_preview/controllers/media_preview_controller.dart
@@ -1,13 +1,14 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:get/get.dart';
 
 import '../../../common/constants.dart';
 import '../../../data/apputils.dart';
 import '../../chat/controllers/chat_controller.dart';
 
-class MediaPreviewController extends GetxController {
+class MediaPreviewController extends FullLifeCycleController with FullLifeCycleMixin {
 
   var userName = Get.arguments['userName'];
 
@@ -85,4 +86,26 @@ class MediaPreviewController extends GetxController {
     debugPrint("length--> ${captionMessage.length}");
     captionMessage[currentPageIndex.value] = value;
   }
+
+  @override
+  void onPaused() {}
+
+  @override
+  void onResumed() {
+    mirrorFlyLog("LifeCycle", "onResumed");
+    if(!KeyboardVisibilityController().isVisible) {
+      if (captionFocusNode.hasFocus) {
+        captionFocusNode.unfocus();
+        Future.delayed(const Duration(milliseconds: 100), () {
+          captionFocusNode.requestFocus();
+        });
+      }
+    }
+  }
+
+  @override
+  void onDetached() {}
+
+  @override
+  void onInactive() {}
 }
