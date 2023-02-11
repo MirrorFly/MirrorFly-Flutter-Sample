0faa6c6861b7e891be1304d5d244ba51f21cbba4
SaravanakumarB_Contus <saravanakumar.b@contus.in>
2023-02-11 11:44:18 +0530

    gallery asset error showing on ui fixed

--- lib/app/modules/gallery_picker/views/gallery_picker_view.dart
+++ lib/app/modules/gallery_picker/views/gallery_picker_view.dart
@@ -27,74 +27,78 @@ class GalleryPickerView extends GetView<GalleryPickerController> {
         child: Column(
           children: [
             Expanded(
-              child: GalleryMediaPicker(
-                childAspectRatio: 1,
-                crossAxisCount: 3,
-                thumbnailQuality: 200,
-                thumbnailBoxFix: BoxFit.cover,
-                singlePick: false,
-                gridViewBackgroundColor: Colors.grey,
-                imageBackgroundColor: Colors.black,
-                maxPickImages: controller.maxPickImages,
-                appBarHeight: 60,
-                selectedBackgroundColor: Colors.black,
-                selectedCheckColor: Colors.black87,
-                selectedCheckBackgroundColor: Colors.white10,
-                pathList: (paths) {
-                  debugPrint("file selected");
-                  controller.addFile(paths);
-                },
-                appBarLeadingWidget: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 15, bottom: 12),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
+              child: FutureBuilder(
+                builder: (context, snapshot) {
+                  return GalleryMediaPicker(
+                    childAspectRatio: 1,
+                    crossAxisCount: 3,
+                    thumbnailQuality: 200,
+                    thumbnailBoxFix: BoxFit.cover,
+                    singlePick: false,
+                    gridViewBackgroundColor: Colors.grey,
+                    imageBackgroundColor: Colors.black,
+                    maxPickImages: controller.maxPickImages,
+                    appBarHeight: 60,
+                    selectedBackgroundColor: Colors.black,
+                    selectedCheckColor: Colors.black87,
+                    selectedCheckBackgroundColor: Colors.white10,
+                    pathList: (paths) {
+                      debugPrint("file selected");
+                      controller.addFile(paths);
+                    },
+                    appBarLeadingWidget: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 15, bottom: 12),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
 
-                        Obx(() {
-                          return Text("${controller.pickedFile.length} / ${controller.maxPickImages}");
-                        }),
+                            Obx(() {
+                              return Text("${controller.pickedFile.length} / ${controller.maxPickImages}");
+                            }),
 
-                        const SizedBox(width: 20,),
-                        GestureDetector(
-                          onTap: () async {
-                            List<String> mediaPath = [];
-                            // media.pickedFile.map((p) {
-                            //   setState(() {
-                            //     mediaPath.add(p.path);
-                            //   });
-                            // }).toString();
-                            if (controller.pickedFile.isNotEmpty) {
-                              // await Share.shareFiles(mediaPath);
-                              Get.toNamed(Routes.mediaPreview, arguments: {
-                                "filePath": controller.pickedFile,
-                                "userName": controller.userName
-                              })?.then((value) {
-                                value != null ? Get.back() : null;
+                            const SizedBox(width: 20,),
+                            GestureDetector(
+                              onTap: () async {
+                                List<String> mediaPath = [];
+                                // media.pickedFile.map((p) {
+                                //   setState(() {
+                                //     mediaPath.add(p.path);
+                                //   });
+                                // }).toString();
+                                if (controller.pickedFile.isNotEmpty) {
+                                  // await Share.shareFiles(mediaPath);
+                                  Get.toNamed(Routes.mediaPreview, arguments: {
+                                    "filePath": controller.pickedFile,
+                                    "userName": controller.userName
+                                  })?.then((value) {
+                                    value != null ? Get.back() : null;
 
-                              });
-                            } else {
-                              Get.back();
-                            }
-                            mediaPath.clear();
-                          },
-                          child: Container(
-                              height: 30,
-                              width: 50,
-                              decoration: BoxDecoration(
-                                color: Colors.transparent,
-                                borderRadius: BorderRadius.circular(10),
-                                border: Border.all(
-                                    color: Colors.blue, width: 1.5),
+                                  });
+                                } else {
+                                  Get.back();
+                                }
+                                mediaPath.clear();
+                              },
+                              child: Container(
+                                  height: 30,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                    color: Colors.transparent,
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border.all(
+                                        color: Colors.blue, width: 1.5),
+                                  ),
+                                  child: const Center(child: Text("Done"))
                               ),
-                              child: const Center(child: Text("Done"))
-                          ),
-                        )
-                      ],
+                            )
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                ),
+                  );
+                }
               ),
             ),
           ],
