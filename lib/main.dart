fb0f14f5bbd6bd4189862f3c23b9f7b8c8f2bff8
SaravanakumarB_Contus <saravanakumar.b@contus.in>
2023-04-05 17:33:49 +0530

    android 13 permission added

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,6 @@ Future<void> main() async {
   Mirrorfly.init(
       baseUrl: 'https://api-uikit-qa.contus.us/api/v1/',
       licenseKey: 'ckIjaccWBoMNvxdbql8LJ2dmKqT5bp',//ckIjaccWBoMNvxdbql8LJ2dmKqT5bp//2sdgNtr3sFBSM3bYRa7RKDPEiB38Xo
-      isTrialLicenceKey: false,
       iOSContainerID: 'group.com.mirrorfly.qa');
   final GoogleMapsFlutterPlatform mapsImplementation =
       GoogleMapsFlutterPlatform.instance;
