07444c411ac1e0ad88e11f0a5117c6e5602b60da
Saravanakumar <saravanakumar.b@contus.in>
2023-12-26 19:03:04 +0530

    added platform check
    Signed-off-by: Saravanakumar <saravanakumar.b@contus.in>

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -303,7 +303,7 @@ class LoginController extends GetxController {
         toToast("Please enter valid mobile number");
         return;
       }
-      if (!(await AppPermission.askNotificationPermission())) {
+      if (Platform.isAndroid && !(await AppPermission.askNotificationPermission())) {
         return;
       }
       // if(mobileNumber.text.length > 9) {
