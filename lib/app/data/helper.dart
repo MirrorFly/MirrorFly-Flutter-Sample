4c61a713cd501e69869ec63ba6e121fc73200b05
Saravanakumar <saravanakumar.b@contus.in>
2023-12-29 14:30:34 +0530

    view all media files issue fixed
    Signed-off-by: Saravanakumar <saravanakumar.b@contus.in>

--- lib/app/data/helper.dart
+++ lib/app/data/helper.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
