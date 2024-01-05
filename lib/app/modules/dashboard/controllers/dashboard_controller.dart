abfafd446f770a11342ecdb9f7daa21c5baa2dd6
Mani Vendhan M <manivendhan.m@contus.in>
2024-01-05 16:47:00 +0530

    #Print to debugPrint

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -149,7 +149,7 @@ class DashboardController extends FullLifeCycleController with FullLifeCycleMixi
     pageNumber = 1;
 
     Mirrorfly.syncCallLogs().then((isSuccess){
-      print("#MirrorflyCall syncCallLogs isSuccess $isSuccess");
+      debugPrint("#MirrorflyCall syncCallLogs isSuccess $isSuccess");
     });
     super.onInit();
   }
