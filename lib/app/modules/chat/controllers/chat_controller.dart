e65edce509bd7a970c14b5fbb124023b8c5398d4
Saravanakumar <saravanakumar.b@contus.in>
2024-02-14 11:05:48 +0530

    logs added
    Signed-off-by: Saravanakumar <saravanakumar.b@contus.in>

--- lib/app/modules/chat/controllers/chat_controller.dart
+++ lib/app/modules/chat/controllers/chat_controller.dart
@@ -2268,6 +2268,7 @@ class ChatController extends FullLifeCycleController
               (message) => message.messageId == chatMessageModel.messageId);
       debugPrint("Media Status Update index of search $index");
       if (index != -1) {
+        LogMessage.d("Media Status Updated",chatMessageModel.toJson());
         // chatMessageModel.isSelected=chatList[index].isSelected;
         chatList[index].mediaChatMessage?.mediaLocalStoragePath(chatMessageModel.mediaChatMessage!.mediaLocalStoragePath.value);
         chatList[index].mediaChatMessage?.mediaDownloadStatus(chatMessageModel.mediaChatMessage!.mediaDownloadStatus.value);
@@ -2277,6 +2278,7 @@ class ChatController extends FullLifeCycleController
         } else if (chatList[index].mediaChatMessage!.mediaDownloadStatus.value == MediaDownloadStatus.storageNotEnough.value) {
           toToast(Constants.insufficientMemoryError);
         }
+        debugPrint("After Media Status Updated ${chatList[index].mediaChatMessage?.mediaLocalStoragePath} ${chatList[index].mediaChatMessage?.mediaDownloadStatus} ${chatList[index].mediaChatMessage?.mediaUploadStatus}");
       }
     }
     if (isSelected.value) {
@@ -3409,51 +3411,5 @@ class ChatController extends FullLifeCycleController
       }
     }
   }
-}
-
-class MyMessageListener implements MessageEventsListener {
-
-
-  @override
-  void onMessageStatusUpdated(String messageId) {
-    LogMessage.d("MyMessageListener", messageId);
-    // Implement your logic for handling message status updates
-  }
-
-  @override
-  void onUpdateUnStarAllMessages() {
-    // Implement your logic for updating unstarred messages
-  }
-
-
-
-  @override
-  void onUploadDownloadProgressChanged(String messageId, int progressPercentage) {
-    // Implement your logic for handling progress changes
-  }
-
-  @override
-  void onMessagesClearedOrDeleted(List<String> messageIds, String jid) {
-    // Implement your logic for handling cleared or deleted messages
-  }
-
-
-
-  @override
-  void updateArchiveUnArchiveChats(String? toUser, bool archiveStatus) {
-    // Implement your logic for updating archive/unarchive chats
-  }
-
-  @override
-  void updateArchivedSettings(bool archivedSettingsStatus) {
-    // Implement your logic for updating archived settings
-  }
-
-  @override
-  void onUpdateBusyStatus(bool status, String? message) {
-    // Implement your logic for updating busy status
-  }
-
-
 }
 
