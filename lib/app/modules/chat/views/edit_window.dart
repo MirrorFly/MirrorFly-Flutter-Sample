159d46468ad58c3bd76eb5624aa8da110d74a81c
Mani Vendhan M <manivendhan.m@contus.in>
2024-04-01 14:14:42 +0530

    #Edit Message and its related changes

--- lib/app/modules/chat/views/edit_window.dart
+++ lib/app/modules/chat/views/edit_window.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:mirror_fly_demo/app/common/extensions.dart';
