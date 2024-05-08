7d6d29342abf07e842d61423a495821c4f320915
Saravanakumar <saravanakumar.b@contus.in>
2024-05-08 11:21:33 +0530

    loading issue fixed

--- lib/app/modules/chat/controllers/contact_controller.dart
+++ lib/app/modules/chat/controllers/contact_controller.dart
@@ -180,7 +180,7 @@ class ContactController extends FullLifeCycleController with FullLifeCycleMixin 
     //fetchUsers(true);
     //}
     usersList(mainUsersList);
-    scrollable(!Constants.enableContactSync);
+    scrollable(!Constants.enableContactSync && mainUsersList.length == 20);
   }
 
   fetchUsers(bool fromSearch, {bool server = false}) async {
@@ -216,7 +216,7 @@ class ContactController extends FullLifeCycleController with FullLifeCycleMixin 
               if (!Constants.enableContactSync) {
                 usersList(list);
                 // if(usersList.length==20) pageNum += 1;
-                scrollable.value = list.length == 20;
+                scrollable(list.length == 20);
               } else {
                 var userlist = mainUsersList
                     .where((p0) => getName(p0).toString().toLowerCase().contains(_searchText.trim().toLowerCase()));
@@ -232,7 +232,7 @@ class ContactController extends FullLifeCycleController with FullLifeCycleMixin 
               if (!Constants.enableContactSync) {
                 usersList.addAll(list);
                 // if(usersList.length==20) pageNum += 1;
-                scrollable.value = list.length == 20;
+                scrollable(list.length == 20);
               } else {
                 usersList(list);
                 scrollable(false);
@@ -260,7 +260,7 @@ class ContactController extends FullLifeCycleController with FullLifeCycleMixin 
               if (!Constants.enableContactSync) {
                 usersList(list);
                 // if(usersList.length==20) pageNum += 1;
-                scrollable.value = list.length == 20;
+                scrollable(list.length == 20);
               } else {
                 var userlist = mainUsersList
                     .where((p0) => getName(p0).toString().toLowerCase().contains(_searchText.trim().toLowerCase()));
@@ -276,7 +276,7 @@ class ContactController extends FullLifeCycleController with FullLifeCycleMixin 
               if (!Constants.enableContactSync) {
                 usersList.addAll(list);
                 // if(usersList.length==20) pageNum += 1;
-                scrollable.value = list.length == 20;
+                scrollable(list.length == 20);
               } else {
                 usersList(list);
                 scrollable(false);
@@ -292,7 +292,7 @@ class ContactController extends FullLifeCycleController with FullLifeCycleMixin 
 
       (!Constants.enableContactSync)
           ? Mirrorfly.getUserList(page: pageNum, search: _searchText,
-          /*metaDataUserList: MetaDataUserList(key: "platform", value: ["flutter"]),*/ //#metaData
+          metaDataUserList: MetaDataUserList(key: "platform", value: ["flutter"]), //#metaData
           flyCallback: callback)
           : Mirrorfly.getRegisteredUsers(fetchFromServer: false, flyCallback: callback);
       /*future.then((data) async {
