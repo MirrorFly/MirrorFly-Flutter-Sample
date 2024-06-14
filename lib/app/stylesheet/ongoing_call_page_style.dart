4c508db2c9fb99568d73a077ba4f140396ccbdd1
Saravanakumar <saravanakumar.b@contus.in>
2024-06-14 18:37:30 +0530

    ongoing call style changes

--- lib/app/stylesheet/ongoing_call_page_style.dart
+++ lib/app/stylesheet/ongoing_call_page_style.dart
@@ -29,8 +29,9 @@ class OngoingCallPageStyle{
     this.callerNameTextStyle = const TextStyle(fontWeight: FontWeight.w500,color: Colors.white,fontSize: 12),
     this.callDurationTextStyle = const TextStyle(fontWeight: FontWeight.normal,color: Colors.white,fontSize: 12),
     this.callStatusTextStyle = const TextStyle(fontWeight: FontWeight.w300,color: Colors.white,fontSize: 14),
-    this.pinnedCallUserTileStyle = const CallUserTileStyle(profileImageSize: 100,backgroundColor: Colors.transparent),
-    this.listCallUserTileStyle = const CallUserTileStyle(profileImageSize: 50),
+    this.pinnedCallUserTileStyle = const CallUserTileStyle(profileImageSize: 100,backgroundColor: Color(
+        0xff0D2852),borderRadius: BorderRadius.zero),
+    this.listCallUserTileStyle = const CallUserTileStyle(profileImageSize: 50,backgroundColor: Color(0xff151F32)),
     this.gridCallUserTileStyle = const CallUserTileStyle(profileImageSize: 60,backgroundColor: Color(0xff151F32),borderRadius: BorderRadius.all(Radius.circular(12)),),
     this.actionButtonsStyle = const ActionButtonStyle(),
     this.disconnectButtonStyle = const ButtonStyle()
