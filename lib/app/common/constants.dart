0c8fe6b2fa6a0ca8e5480b25636aa41ca5bf79bc
Mani Vendhan M <manivendhan.m@contus.in>
2024-12-20 14:19:14 +0530

    #Bg for Image view

--- lib/app/common/constants.dart
+++ lib/app/common/constants.dart
@@ -288,7 +288,7 @@ const String forwardMedia = "assets/logos/forward_media.svg";
 const String arrowDown = "assets/logos/arrow_down.svg";
 const String arrowUp = "assets/logos/arrow_up.svg";
 
-const String mediaBg = "assets/logos/ic_baloon.svg";
+const String mediaBg = "assets/logos/ic_baloon.png";
 
 //contact sync
 const String syncIcon = "assets/logos/sync.svg";
